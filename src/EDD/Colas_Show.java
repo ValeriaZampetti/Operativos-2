c7aa6deadf9e1b4c560c25996870ba371e306744
val <valeriazampettirubino.com>
2024-02-21 17:49:57 -0400

    cola de prioridad para agregar personaje

--- src/EDD/Colas_Show.java
+++ src/EDD/Colas_Show.java
@@ -6,6 +6,7 @@ package EDD;
 
 import Enums.Resultado_Pelea;
 import Personajes.PersonajeJugable;
+import Utils.Funciones;
 import operativos.pkg2.Data_Pelea;
 
 /**
@@ -13,13 +14,13 @@ import operativos.pkg2.Data_Pelea;
  * @author Sergionx
  */
 public class Colas_Show {
-
+    
     private final Cola<PersonajeJugable> cola_Alta;
     private final Cola<PersonajeJugable> cola_Media;
     private final Cola<PersonajeJugable> cola_Baja;
-
+    
     private final Cola<PersonajeJugable> cola_Refuerzo;
-
+    
     private final String showId; //Debería verse como AVATAR
 
     public Colas_Show(String showId) {
@@ -27,10 +28,10 @@ public class Colas_Show {
         this.cola_Media = new Cola();
         this.cola_Baja = new Cola();
         this.cola_Refuerzo = new Cola();
-
+        
         this.showId = showId;
     }
-
+    
     public PersonajeJugable escojerPersonaje() {
         if (!cola_Alta.EsVacio()) {
             return cola_Alta.Desencolar();
@@ -38,28 +39,45 @@ public class Colas_Show {
             return cola_Media.Desencolar();
         }
         return cola_Baja.Desencolar();
+        
     }
-
+    
     public void decidir_Accion_Pelea(Resultado_Pelea resultado_Pelea, PersonajeJugable personaje) {
         switch (resultado_Pelea) {
             case VICTORIA -> {
 //                TODO - Mostrar ganador y guardar en lista de ganadores
             }
-
+            
             case EMPATE -> {
                 cola_Alta.Encolar(personaje);
             }
-
+            
             case INCAPAZ -> {
+                var personaje_refuerzo = cola_Refuerzo.Desencolar();
+                if (Funciones.try_Probability(40)) {
+                    cola_Alta.Encolar(personaje_refuerzo); 
+                } else {
+                    cola_Refuerzo.Encolar(personaje_refuerzo);
+                }
                 cola_Refuerzo.Encolar(personaje);
             }
-
+            
             default ->
                 throw new AssertionError();
         }
     }
     
-
+    public void agregar_Personaje(PersonajeJugable personaje){
+        switch (personaje.getRareza()) {
+            case ALTA -> cola_Alta.Encolar(personaje);
+            case MEDIA -> cola_Media.Encolar(personaje);
+            case BAJA -> cola_Baja.Encolar(personaje);
+            default -> throw new AssertionError();
+        }
+    
+    }
+    
+    
     public void actualizarContadores() {
     }
 }
