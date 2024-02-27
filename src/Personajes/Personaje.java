bed993dcb0fc485cdefe055647d5a5eea17be250
Sergionx <sergio180203@gmail.com>
2024-02-27 12:11:29 -0400

    Refactor Personaje constructor to include a "nombre" parameter

--- src/Personajes/Personaje.java
+++ src/Personajes/Personaje.java
@@ -9,17 +9,19 @@ package Personajes;
  * @author valeriazampetti
  */
 public class Personaje {
-    
+
+    public String nombre;
     public int puntos_Habilidades;
     public int puntos_Vida;
     public int puntos_Fuerza;
     public int puntos_Agilidad;
 
-    public Personaje(int puntos_Habilidades, int puntos_Vida, int puntos_Fuerza, 
-            int puntos_Agilidad) {
-        this.puntos_Habilidades =  puntos_Habilidades;
+    public Personaje(String nombre, int puntos_Habilidades, int puntos_Vida,
+            int puntos_Fuerza, int puntos_Agilidad) {
+        this.nombre = nombre;
+        this.puntos_Habilidades = puntos_Habilidades;
         this.puntos_Vida = puntos_Vida;
         this.puntos_Fuerza = puntos_Fuerza;
         this.puntos_Agilidad = puntos_Agilidad;
-    }  
+    }
 }
